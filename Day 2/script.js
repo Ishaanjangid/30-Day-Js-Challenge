d515b70c0737c3abfae1d17e22ad32114e430058
Ishaanjangid <ishaanjangid035@gmail.com>
2024-07-14 10:10:02 +0530

    Complete Day 2 Task

---
+++ Day 2/script.js
@@ -0,0 +1,51 @@
+// ___Arithmetic Operations___
+// Task_1
+
+let num1 = 6;
+let num2 = 4;
+
+console.log(num1 + num2);
+
+// Task_2
+
+let num3 = 10;
+let num4 = 4;
+
+console.log(num3 - num4);
+
+// Task _3
+let num5 = 20;
+let num6 = 3;
+
+console.log(num5 % num6);
+
+// __Assignment Operator__
+num1 += 10;
+console.log(num1);
+
+num5 -= 33;
+console.log(num5);
+
+
+// __Compersion Operator__
+console.log(54>65);
+console.log(765<23);
+
+console.log(643>=654);
+console.log(34<=5);
+console.log('');
+
+console.log(4==4);
+console.log(4==='4');
+
+console.log('');
+// __Logical Operator__
+console.log(5435>5435 && 435<6435);
+console.log(5435>5435 || 435<6435);
+console.log(! 435<6435);
+
+// __Ternery Operator__
+
+let num  = -54;
+
+(num >= 0) ? console.log(`${num} is positive`) : console.log(`${num} is negative`);
